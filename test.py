14ddbcf55e924ce1fd7682e42bc5c1b70e593717
JinkuanZhu <42997811+JinkuanZhu@users.noreply.github.com>
2019-11-14 21:41:02 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-import cv2
-import os
-from DeepLearning.python import int_fun
-for f in os.listdir('E:\\KITTI2012\\OneShot'):
-    im = cv2.imread('E:\\KITTI2012\\OneShot\\' + f)
-    im = cv2.resize(im, (900, 800))
-    cv2.imwrite('E:\\KITTI2012\\OneShot\\' + f, im)
